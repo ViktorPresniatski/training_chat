408101c552b26f35c4c064213f87da378dc0f8e2
Dima Grechko <d.grechko@sumatosoft.com>
2018-01-15 15:43:37 +0300

    change fetch to axios

--- frontend/src/javascripts/actions/conversationActions.js
+++ frontend/src/javascripts/actions/conversationActions.js
@@ -1,7 +1,6 @@
 import { TEST_ACTION_CONSTANT, GET_CONVERSATIONS_SUCCESS } from '../constants';
-
 export const testMessage = () => ({type: TEST_ACTION_CONSTANT});
-
+let axios = require('axios');
 export const getConversations = (conversations) => (
   {
     type: GET_CONVERSATIONS_SUCCESS,
@@ -10,12 +9,13 @@ export const getConversations = (conversations) => (
 
 export const conversationsApiCall = () => {
   return (dispatch) => {
-    fetch('http://localhost:4000/conversations', {
-      method: 'GET',
-   })
+   axios({
+      method: 'get',
+      url: 'http://localhost:4000/conversations'
+    })
     .then(response => {
       if (response.status >= 200 && response.status < 300) {
-        return response.json();
+        return response.data;
       } else {
         const error = new Error(response.statusText);
         throw error;
